1fc7cb5a811c02208b04ea382a862a294d968b90
Vishwanath <69393019+MindFaktur@users.noreply.github.com>
2023-02-26 08:16:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-from generate_report import gen_report
+from generate_report import GenerateReport
 
 app = FastAPI()
 
 
 @app.get("/trigger_report")
 def trigger_report():
-    return gen_report()
+    return GenerateReport().gen_report()
 
 
 @app.get("/get_report/{report_id}")
 def get_report(report_id: str):
-    return FileResponse(report_id)
+    if GenerateReport().get_report(report_id):
+        return FileResponse(report_id)
+    else:
+        return {
+            "success": "False",
+            "message": "Report id does not exist, please generate new",
+        }
